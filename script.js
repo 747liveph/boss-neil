38ab507a38343e60b14d7295c6065a4c87815eec
747liveph <dekster.ocampo@gmail.com>
2026-08-07 10:39:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let online = 8426;
-let jackpot = 15.80;
-
-setInterval(() => {
-
-    online += Math.floor(Math.random() * 5) + 1;
-
-    document.getElementById("playersOnline").textContent =
-        online.toLocaleString() + "+";
-
-    jackpot += Math.random() * 0.03;
-
-    document.getElementById("jackpotWon").textContent =
-        "₱" + jackpot.toFixed(2) + "M+";
-
-    const times = [
-        "1-3 Mins",
-        "2-4 Mins",
-        "2-5 Mins",
-        "3-5 Mins"
-    ];
-
-    document.getElementById("withdrawTime").textContent =
-        times[Math.floor(Math.random() * times.length)];
-
-}, 2500);
 // ===============================
 // BOSS NEIL LANDING PAGE
 // script.js
